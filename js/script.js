113a0fde38bb224c1e863e67b70642afe33d1e81
HeartOfAutumn <ngominhthuworks@gmail.com>
2020-09-01 14:49:52 +0700

    Complete products

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+
 document.getElementsByClassName("menu-toggle")[0].addEventListener('click', function(){
     document.getElementsByTagName('nav')[0].classList.toggle('active');
 });
+
+$(document).ready(function(){
+    $('.list').click(function(){
+        $('.list').removeClass('active');
+        $(this).addClass('active');
+        const value = $(this).attr('data-filter');
+        if(value == 'all'){
+            $('.card').show('1000');
+        }
+        else{
+            $('.card').not('.'+value).hide('1000');
+            $('.card').filter('.'+value).show('1000');
+        }
+    });
+
+    let zero = 0;
+    $(window).on('scroll', function(){
+        $('header').toggleClass('hide', $window.scrollTop() > zero);
+        zero = $window.scrollTop();
+    })
+});
+
+
